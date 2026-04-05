b17080631e56ea80003012fd204c748c1f571aca
liseth <liseth052009@gmail.com>
2026-04-05 16:00:11 -0500

    se agrego la interactividad entre las activitys, adicional la busqueda de veterinarias por maps, la opcion de compartir el historial y carnet de vacunas por pdf y las notificaciones en tiempo real

--- app/src/main/java/com/liseth/miprimeraapp/BusquedaActivity.java
+++ app/src/main/java/com/liseth/miprimeraapp/BusquedaActivity.java
@@ -2,12 +2,50 @@ package com.liseth.miprimeraapp;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 
-public class BusquedaActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class BusquedaActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busqueda);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng bogota = new LatLng(4.6097, -74.0817);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bogota, 13f));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(4.6150, -74.0800))
+                .title("Veterinaria Animal Care"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(4.6050, -74.0900))
+                .title("Clínica Veterinaria San Martín"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(4.6200, -74.0750))
+                .title("Vet Salud Mascotas"));
     }
 }
